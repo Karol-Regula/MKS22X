6e2b5bb5b00c950716f5fa99fafa8d8a8c27082a
Karol R <kregula@stuy.edu>
2016-02-07 19:27:11 -0500

    Created new repo for homework, began work on first assignment: 01recursion. Created name() method that returns name.

---
+++ 01recursion/Recursion.java
@@ -0,0 +1,12 @@
+public class Recursion{
+
+    public static void main(String[]args){
+	Recursion r1 = new Recursion();
+	System.out.println(r1.name());
+    }
+
+    public String name(){
+	return "Regula,Karol";
+    }
+
+}
