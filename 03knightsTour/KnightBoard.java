eaf3b8094b6c68040b46e2c3597230ae8024d623
Karol R <kregula@stuy.edu>
2016-02-22 22:10:24 -0500

    Finished KnightBoard (I hope), tested 5x5, 6x6, 7x7, 3x7 and 5x6, all seem to work, revised printSolution() method to include better formatting.

--- 03knightsTour/KnightBoard.java
+++ 03knightsTour/KnightBoard.java
@@ -1,25 +1,44 @@
 import java.io.*;//make this specific later
 public class KnightBoard{
     int[][] board;
-    int size;
+    int rows;
+    int cols;
     int counter;
     
     public static void main(String[]args){
-	KnightBoard b1 = new KnightBoard(4);
+	/*
+	KnightBoard b1 = new KnightBoard(5,5);
 	b1.solve();
 	b1.printSolution();
+	KnightBoard b2 = new KnightBoard(6,6);
+	b2.solve();
+	b2.printSolution();
+	KnightBoard b3 = new KnightBoard(5,7);
+	b3.solve();
+	b3.printSolution();
+	KnightBoard b4 = new KnightBoard(3,5);
+	b4.solve();
+	b4.printSolution();
+	KnightBoard b5 = new KnightBoard(3,7);
+	b5.solve();
+	b5.printSolution();
+	KnightBoard b6 = new KnightBoard(5,6);
+	b6.solve();
+	b6.printSolution();
+	*/
     }
 
-    public KnightBoard(int side){
-	size = side;
-	board = new int[size][size];
+    public KnightBoard(int rowsNum, int colsNum){
+	rows = rowsNum;
+	cols = colsNum;
+	board = new int[rows][cols];
 	fillBoard();
-	counter = 0;
+	counter = 1;
     }
 
     public void fillBoard(){
-	for (int i = 0; i < size; i++){
-	    for (int j = 0; j < size; j++){
+	for (int i = 0; i < rows; i++){
+	    for (int j = 0; j < cols; j++){
 		board[i][j] = 0;
 	    }
 	}
@@ -30,70 +49,53 @@ public class KnightBoard{
     }
 
     public boolean solveHelper(int row, int col){
-	//somehow backtracking doesn't work at all, not even once...
-	//!!!???
 	if (! onBoard(row, col)){
 	    return false;
 	}
-	printSolution();
-	System.out.println(counter);
-	if (counter == size * size && board[row][col] == 1){//check counter increments, if errors occur, probably is one off
-	    System.out.println(counter + " " + row + " "  + col);
+	//printSolution();
+	//System.out.println(counter);
+	//System.out.println("----------------------------");
+	board[row][col] = counter;
+	counter++;
+	if (counter == (rows * cols) + 1){//check counter increments, if errors occur, probably is one off
+	    //System.out.println(counter + " " + row + " "  + col);
 	    return true;//this means the entire board has been filled
 	}
-	//also make sure the rows and cols are not switched, althought that shouldn't make that much of a difference
-	//commence the eight 'if' statements
 	//up left
-	if(onBoard(row + 2, col - 1) && board[row + 2][col - 1] ==0){
-	    board[row + 2][col - 1] = counter;
-	    counter++;
-	    return(solveHelper(row + 2, col - 1));
+	if(onBoard(row + 2, col - 1) && board[row + 2][col - 1] ==0 && solveHelper(row + 2, col - 1)){
+	    return true;
 	}
 	//up right
-	if(onBoard(row + 2, col + 1) && board[row + 2][col + 1] ==0){
-	    board[row + 2][col + 1] = counter;
-	    counter++;
-	    return(solveHelper(row + 2, col + 1));
+	if(onBoard(row + 2, col + 1) && board[row + 2][col + 1] ==0 && solveHelper(row + 2, col + 1)){
+	    return true;
 	}
 	//down left
-	if(onBoard(row - 2, col - 1) && board[row - 2][col - 1] ==0){
-	    board[row - 2][col - 1] = counter;
-	    counter++;
-	    return(solveHelper(row - 2, col - 1));
+	if(onBoard(row - 2, col - 1) && board[row - 2][col - 1] ==0 && solveHelper(row - 2, col - 1)){
+	    return true;
 	}
 	//down right
-	if(onBoard(row - 2, col + 1) && board[row - 2][col + 1] ==0){
-	    board[row - 2][col + 1] = counter;
-	    counter++;
-	    return(solveHelper(row - 2, col + 1));
+	if(onBoard(row - 2, col + 1) && board[row - 2][col + 1] ==0 && solveHelper(row - 2, col + 1)){
+	    return true;
 	}
 	//right up
-	if(onBoard(row + 1, col + 2) && board[row + 1][col + 2] ==0){
-	    board[row + 1][col + 2] = counter;
-	    counter++;
-	    return(solveHelper(row + 1, col + 2));
+	if(onBoard(row + 1, col + 2) && board[row + 1][col + 2] ==0 && solveHelper(row + 1, col + 2)){
+	    return true;
 	}
 	//right down
-	if(onBoard(row - 1, col + 2) && board[row - 1][col + 2] ==0){
-	    board[row - 1][col + 2] = counter;
-	    counter++;
-	    return(solveHelper(row - 1, col + 2));
+	if(onBoard(row - 1, col + 2) && board[row - 1][col + 2] ==0 && solveHelper(row - 1, col + 2)){
+	    return true;
 	}
 	//left up
-	if(onBoard(row + 1, col - 2) && board[row + 1][col - 2] ==0){
-	    board[row + 1][col - 2] = counter;
-	    counter++;
-	    return(solveHelper(row + 1, col - 2));
+	if(onBoard(row + 1, col - 2) && board[row + 1][col - 2] ==0 && solveHelper(row + 1, col - 2)){
+	    return true;
 	}
 	//left down
-	if(onBoard(row - 1, col - 2) && board[row - 1][col - 2] ==0){
-	    board[row - 1][col - 2] = counter;
-	    counter++;
-	    return(solveHelper(row - 1, col - 2));
+	if(onBoard(row - 1, col - 2) && board[row - 1][col - 2] ==0 && solveHelper(row - 1, col - 2)){
+	    return true;
 	}
-	//need the backtracking part
 	board[row][col] = 0;
 	counter--;
+	return false;
 	/*
 	for (int i = 0; i < board.length; i++){
 	    for (int j = 0; j < board.length; j++){
@@ -106,7 +108,6 @@ public class KnightBoard{
 	//need to somehow stop if from trying the same one again and then undoing over and over again, this is badly written logically
 	solveHelper(row, col, counter);
 	*/
-	return false;
     }
 
     public boolean onBoard(int row, int col){
@@ -114,18 +115,23 @@ public class KnightBoard{
 	    board[row][col] = board[row][col];
 	    return true;
 	}catch (ArrayIndexOutOfBoundsException e){
-	    System.out.println("Out of bounds.");
+	    //System.out.println("Out of bounds.");
 	    return false;
 	}
 	//try - catch checks if new spot is on the board
     }
     
     public void printSolution(){
-	for (int i = 0; i < size; i++){
-	    for (int j = 0; j < size; j++){
-		System.out.print(board[i][j] + " ");
+	for (int i = 0; i < rows; i++){
+	    for (int j = 0; j < cols; j++){
+		if (board[i][j] < 10){
+		    System.out.print("_" + board[i][j] + " ");
+		}else{
+		    System.out.print(board[i][j] + " ");
+		}
 	    }
 	    System.out.println();
 	}
+	System.out.println();
     }
 }
